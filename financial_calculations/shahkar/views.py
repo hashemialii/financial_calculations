caca92cc612bcc70686edf89fa9fcd2b95a61692
ali <alihashemi9675@gmail.com>
2024-10-14 15:48:26 +0330

    complete CRUD

--- financial_calculations/shahkar/views.py
+++ financial_calculations/shahkar/views.py
@@ -17,7 +17,6 @@ class IncomeCalculationsViewSet(viewsets.ModelViewSet):
                 {'error': 'Please provide year, month, and amount.'},
                 status=status.HTTP_400_BAD_REQUEST
             )
-
         basic_instance = IncomeService.create_basic_entry(request.data)
         income_instance = IncomeService.create_income_entry(basic_instance)
 
@@ -25,21 +24,16 @@ class IncomeCalculationsViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def update(self, request, *args, **kwargs):
-        income_instance = self.get_object()  # دریافت رکورد موردنظر
-
-        # دریافت مقدار جدید از درخواست
+        income_instance = self.get_object()
         amount = request.data.get('amount')
         if amount is None:
             return Response({'error': 'Amount is required.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # به‌روزرسانی رکورد با استفاده از مقدار جدید
         updated_instance = IncomeService.update_amount(income_instance, amount)
-
         serializer = self.get_serializer(updated_instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def destroy(self, request, *args, **kwargs):
         income_instance = self.get_object()
         basic_instance = income_instance.basic
-        basic_instance.delete()  # حذف رکورد وابسته
+        basic_instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
