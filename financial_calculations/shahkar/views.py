7474e7f4b812180749ce03f97d37c0de21a87a7b
ali <alihashemi9675@gmail.com>
2024-10-27 18:14:21 +0330

    change models.py

--- financial_calculations/shahkar/views.py
+++ financial_calculations/shahkar/views.py
@@ -14,7 +14,7 @@ class IncomeCalculationsViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        basic_instance = IncomeService.create_basic_entry(request.data)
+        basic_instance = IncomeService.create_basic_entry(serializer.validated_data)
 
         income_instance = IncomeService.create_income_entry(basic_instance)
 
@@ -22,23 +22,3 @@ class IncomeCalculationsViewSet(viewsets.ModelViewSet):
         basic_instance.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def update(self, request, *args, **kwargs):
-        print(request.data)
-        income_instance = self.get_object()
-        print(income_instance)
-        amount = request.data.get('amount')
-        # partial update
-        # upddate amoynt only
-        if amount is None:
-            return Response({'error': 'Amount is required.'}, status=status.HTTP_400_BAD_REQUEST)
-        updated_instance = IncomeService.update_amount(income_instance, amount)
-        serializer = self.get_serializer(updated_instance)
-        print(serializer.data)
-        return Response(serializer.data)
-
-    def destroy(self, request, *args, **kwargs):
-        income_instance = self.get_object()
-        basic_instance = income_instance.basic
-        basic_instance.delete()
-        return Response({f'{income_instance.basic} was deleted'})
