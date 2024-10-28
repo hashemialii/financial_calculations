944de74a65dac72534d0369a01f84e99555e2797
ali <alihashemi9675@gmail.com>
2024-10-28 12:13:00 +0330

    change models.py

--- financial_calculations/shahkar/services.py
+++ financial_calculations/shahkar/services.py
@@ -41,3 +41,20 @@ class IncomeService:
             month=fiscal_month
         )
         return income_instance
+
+    @staticmethod
+    def update_basic_entry(basic_instance, data):
+        basic_instance.year = data.get('year', basic_instance.year)
+        basic_instance.month = data.get('month', basic_instance.month)
+        basic_instance.amount = data.get('amount', basic_instance.amount)
+        basic_instance.save()
+        return basic_instance
+
+    @staticmethod
+    def update_income_entry(basic_instance):
+        income_instance = basic_instance.basic
+        income_instance.income = IncomeCalculator.calculate(
+            basic_instance.year, basic_instance.amount
+        )
+        income_instance.save()
+        return income_instance
--- financial_calculations/shahkar/views.py
+++ financial_calculations/shahkar/views.py
@@ -23,3 +23,18 @@ class IncomeCalculationsViewSet(viewsets.ModelViewSet):
         basic_instance.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        basic_instance = IncomeService.update_basic_entry(instance, serializer.validated_data)
+
+        income_instance = IncomeService.update_income_entry(basic_instance)
+
+        basic_instance.basic = income_instance
+        basic_instance.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
